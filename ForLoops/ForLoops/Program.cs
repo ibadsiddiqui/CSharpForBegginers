6decdc16ee593a79c0f310c27f43a7ca1cd81339
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-03 22:09:04 +0500

    For loops lesson made

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine(i.ToString());
+                    break;
+                }
+            }
+            Console.Read();
+
+
+            for (int myValue = 0; myValue < length; myValue++)
+            {
+                // press tab during snippets
+            }
+        }
+    }
+}
--- lesson1/HelloWorld/HelloWorld/Program.cs
+++ lesson1/HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+           
             Console.WriteLine("Hello World");
+            Console.Read();   
         }
     }
 }
