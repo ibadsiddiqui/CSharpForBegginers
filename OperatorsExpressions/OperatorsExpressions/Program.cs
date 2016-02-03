5b1c34528bce0672f08c4f1973d5a50572f7eeb6
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-03 21:47:32 +0500

    Operators and Expressions lesson made

---
+++ OperatorsExpressions/OperatorsExpressions/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // +operator .. adds
+            // =operators variables assignment
+            // == operators .. method invocations.. ex Console.WriteLine()
+            // . operator ... member access
+
+            int x, y, a, b;
+            // Assignment operator
+            x = 10;
+            a = 5;
+            b = 6;
+            y = 8;
+
+            // + operator
+            x = x + a;
+
+            // * operator
+            x = x * a;
+
+            // "/" operator
+            x = x / a;
+
+            // there are many operators used to evaluate
+            
+            // equality operators
+            if (x == y)
+            
+            // greater than and less than operators
+            if(x > y)
+            if(x < y)
+
+             //Conditional operators
+            if( (x > y) || (a < b))
+            if( (x > y) && (a < b))
+                        
+            
+        }
+    }
+}
