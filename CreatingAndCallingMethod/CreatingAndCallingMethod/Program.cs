41c60789076408f4bc4e9fba4aaa9c3d64ca03d9
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-04 21:38:00 +0500

    creating and Calling Methods(Functions) in our programs

---
+++ CreatingAndCallingMethod/CreatingAndCallingMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingAndCallingMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string h1 = hello();
+            Console.WriteLine(h1);
+
+        }
+
+        private static String hello()
+        {
+            return "Hello World";
+        }
+    }
+}
