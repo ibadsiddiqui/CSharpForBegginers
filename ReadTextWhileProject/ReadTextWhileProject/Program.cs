860b5c0bff7204c3e9804de80570d4cd751866fd
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-04 22:27:08 +0500

    Values.txt Moved from outside to the Bin folder of project

--- ReadTextWhileProject/ReadTextWhileProject/Program.cs
+++ ReadTextWhileProject/ReadTextWhileProject/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace ReadTextWhileProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            StreamReader myReader = new StreamReader("Values.txt");
+            String line = "";
+
+            while(line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null)
+                    Console.WriteLine(line);
+                
+            }
+            myReader.Close();
+
+            Console.Read();
         }
     }
 }
