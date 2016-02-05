72b6230b55ec7f5f24a3a2a75106fafbad245343
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-05 16:34:15 +0500

    DateAndTime project created and introducted DateTime.Now Function

---
+++ DateAndTime/DateAndTime/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            Console.WriteLine(myValue.ToString());
+            
+            Console.Read();
+        }
+    }
+}
