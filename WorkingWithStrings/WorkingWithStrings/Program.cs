cd2d1251aad62be0b3d8351c0800ef00c58b0a8f
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-05 15:08:34 +0500

    WorkingWithString created

---
+++ WorkingWithStrings/WorkingWithStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myString = "Go to you C:\\ drive";
+
+            Console.WriteLine(myString);
+
+            Console.Read();
+        }
+    }
+}
