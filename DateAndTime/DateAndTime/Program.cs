e512708995423ec7c433cf7b9a41e3547c6bd392
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-05 18:31:51 +0500

    Birthday subtraction helper method created

--- DateAndTime/DateAndTime/Program.cs
+++ DateAndTime/DateAndTime/Program.cs
@@ -10,10 +10,29 @@ namespace DateAndTime
     {
         static void Main(string[] args)
         {
-            DateTime myValue = DateTime.Now;
-            Console.WriteLine(myValue.ToString());
-            
+            //DateTime myValue = DateTime.Now;
+            //Console.WriteLine(myValue.ToString());
+
+            //Console.WriteLine(myValue.ToShortDateString());
+            //Console.WriteLine(myValue.ToShortTimeString());
+
+            //Console.WriteLine(myValue.ToLongDateString());
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+            //Console.WriteLine(myValue.AddHours(-3).ToLongTimeString());
+
+            //DateTime myValue = new DateTime(1996,05,25);
+            //Console.WriteLine(myValue.ToShortDateString());
+        
+            DateTime myBirthday = DateTime.Parse("1996/05/25");  //Verify your Birth date
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);  // Subtract the time now with your Birth date
+            Console.WriteLine(myAge.TotalHours); //Tells Hours
+
             Console.Read();
+
         }
     }
 }
