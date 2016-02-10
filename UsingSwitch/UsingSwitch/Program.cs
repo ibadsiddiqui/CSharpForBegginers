7b06ed90376f76efacb515fce2586894175c5d91
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-10 23:11:55 +0500

    use of switch introduced

---
+++ UsingSwitch/UsingSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type i a super herp's name tp see his nickname:");
+            string value = Console.ReadLine();
+
+            switch (value)
+            {
+                case "BATMAN":
+                    Console.Write("Caped Crusador");
+                    break;
+                case "SUPERMAN":
+                    Console.WriteLine("Man of Steel");
+                    break;
+                case "GREENLANTERN":
+                    Console.WriteLine("Emrald Knight");
+                    break;
+                default:
+                    Console.WriteLine("Does not compute.");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
