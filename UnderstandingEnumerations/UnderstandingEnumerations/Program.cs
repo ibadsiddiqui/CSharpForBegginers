eb9471cc75aceff92e9618901949ac5dd576fcbe
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-10 23:28:28 +0500

    switch used with generic enumeration

---
+++ UnderstandingEnumerations/UnderstandingEnumerations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingEnumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.ForegroundColor = ConsoleColor.DarkRed; //Changes text color to red
+            //Console.WriteLine("Hello World");
+
+            Console.WriteLine("Enter Any name of super hero:");
+            string heroName = Console.ReadLine();
+            SuperHero value;
+
+            if(Enum.TryParse < SuperHero > (heroName,true,out value))
+            switch (value)
+	        {
+		        case SuperHero.Batman:
+                    Console.WriteLine("Caped Crusader");
+                    break;
+                case SuperHero.Superman:
+                    Console.WriteLine("Man of Steel");
+                    break;
+                case SuperHero.GreenLantern:
+                    Console.WriteLine("Emrald Knight");
+                    break;
+                default:
+                    break;
+	        }
+            else
+                Console.WriteLine("Does not compute");
+	        
+            Console.Read();
+        }
+    }
+    enum SuperHero
+    {
+        Batman,
+        Superman,
+        GreenLantern,
+    }
+}
