3381c778458399a9e1525bef778e5c6998908fd9
Ibad Siddiqui <ibadsiddiqui01@outlook.com>
2016-02-13 09:37:05 +0500

    ASP.NET intro

---
+++ ASP.NETevents/ASP.NETevents/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ASP.NETevents.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Button ID="MyButton" runat="server" OnClick="Button1_Click" />
+        <asp:Label ID="MyLabel" runat="server"></asp:Label>
+    
+    </div>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ ASP.NETevents/ASP.NETevents/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASP.NETevents
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MyButton.Click += MyButton_Click;
+        }
+
+        void MyButton_Click(object sender, EventArgs e)
+        {
+               //throw new NotImplementedException();
+            MyLabel.Text = "Hello world";
+        }
+    }
+}
